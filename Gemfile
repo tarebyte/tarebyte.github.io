282cab2a30cadd099e1cc3ae5094de3decd6e341
Mark Tareshawty <tarebyte@gmail.com>
2013-04-22 03:55:28 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~>3.0.13"
+gem "middleman-livereload"
+gem "middleman-gh-pages"
---
+++ config.rb
@@ -0,0 +1,33 @@
+# LiveReload
+activate :livereload
+
+# Personal Configs
+set :haml, :format => :html5
+
+# Asset Directories
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
