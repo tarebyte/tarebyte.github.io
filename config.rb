76dc5d899ed78b5c923930a80b7b8ae5816f0a07
Mark Tareshawty <tarebyte@gmail.com>
2013-12-11 11:19:06 -0500

    new config with blog

--- config.rb
+++ config.rb
@@ -1,37 +1,56 @@
-# LiveReload
-activate :livereload
+# Some settings borrowed from http://github.com/shkm/middleman-neato
 
-# Personal Configs
-set :haml, :format => :html5
+# Meta Data
+set :meta, {
+  title: 'Welcome Back',
+  url:   'http://tarebyte.github.io'
+}
+
+# Syntax Highlighting
+activate :syntax
+set :syntax_theme, Rouge::Themes::Base16
 
 # Asset Directories
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir,      'assets/stylesheets'
+set :js_dir,       'assets/javascripts'
+set :images_dir,   'assets/images'
+set :font_fir,     'assets/fonts'
+set :partials_dir, 'partials'
 
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+after_configuration do
+  # Adding Bower to asset path
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
 
+# Da Blog
+activate :blog do |blog|
+  blog.prefix            = 'blog'
+  blog.permalink         = '{year}/title.html'
+  blog.taglink           = 'tags/{tag}.html'
+  blog.default_extension = ".md"
+
+  blog.paginate = true
+  blog.per_page = 10
+end
+
+configure :development do
+  activate :livereload, apply_js_live: false, apply_css_live: false
+end
+
+configure :build do
+  activate :relative_assets
+  activate :directory_indexes
+  activate :asset_hash
+
+  activate :minify_html, remove_input_attributes: false
+  activate :minify_css
+  activate :minify_javascript
+  activate :image_optim
+  activate :gzip, exts: %w(.js .css .html .html .svg .ttf .otf .woff .eot)
+end
+
+# Deploy build to master branch
 activate :deploy do |deploy|
   deploy.method = :git
   deploy.branch = "master"
