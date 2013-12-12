7088c9afd5feae58453a7efe3be64d737bd4452c
Mark Tareshawty <tarebyte@gmail.com>
2013-12-12 00:23:09 -0500

    done with the header for now

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,6 +1,7 @@
 //= require jquery/jquery
 //= require jquery.scrollTo/jquery.scrollTo
 //= require jquery.localScroll/jquery.localScroll
+//= require ss-social
 
 $(document).ready(function() {
 	$('nav').localScroll({duration:800});
